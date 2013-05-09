894953ae9040497f4be071e4be208486d6036ba8
Marcilio Leite <marcilioleite@live.com>
2013-05-09 02:48:18 -0300

    First commit

--- __init__.py
+++ __init__.py
@@ -3,9 +3,10 @@ import MySQLdb
 con = MySQLdb.connect('127.0.0.1', 'root', '')
 con.select_db('sopjd')
 cursor = con.cursor()
-cursor.execute('select * from itens')
-rs = cursor.fetchone()
+cursor.execute('show tables')
+rs = cursor.fetchall()
 
 #num_fields = len(cursor.description)
 field_names = [i[0] for i in cursor.description]
 print field_names
+print rs[1]
