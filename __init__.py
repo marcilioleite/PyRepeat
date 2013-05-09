ef46dd607e24439e2a5f791b344dc00da89bb0fb
Marcilio Leite <marcilioleite@live.com>
2013-05-09 13:57:39 -0300

    Modificado

--- __init__.py
+++ __init__.py
@@ -6,15 +6,20 @@ cursor = con.cursor()
 
 # Get all tables
 #
-#cursor.execute('show tables')
-#tables = cursor.fetchall()
-#for table in tables:
-#    print table 
+cursor.execute('show tables')
+tables = cursor.fetchall()
+for table in tables:
+    print "Table '%s'" % table
+    # Get all columns from a table
+    #
+    cursor.execute("describe %s" % table)
+    #num_fields = len(cursor.description)
+    columns_metadata = [i[0] for i in cursor.description]
+    print columns_metadata
+    columns = cursor.fetchall()
+    for column in columns:
+        print column
 
-# Get all columns from a table
-#
-#cursor.execute('select * from itens')
-#num_fields = len(cursor.description)
-#columns = [i[0] for i in cursor.description]
-#for column in columns:
-#    print column
+
+# Insert Batch
+#INSERT INTO tbl_name (a,b,c) VALUES(1,2,3),(4,5,6),(7,8,9);
