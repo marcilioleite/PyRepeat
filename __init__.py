6ceb9d96cc7d83b88c8875bfe514bf48b80f13e9
Marcilio Leite <marcilioleite@live.com>
2013-05-09 02:56:41 -0300

    Pegando tabelas e colunas

--- __init__.py
+++ __init__.py
@@ -3,10 +3,18 @@ import MySQLdb
 con = MySQLdb.connect('127.0.0.1', 'root', '')
 con.select_db('sopjd')
 cursor = con.cursor()
-cursor.execute('show tables')
-rs = cursor.fetchall()
 
+# Get all tables
+#
+#cursor.execute('show tables')
+#tables = cursor.fetchall()
+#for table in tables:
+#    print table 
+
+# Get all columns from a table
+#
+#cursor.execute('select * from itens')
 #num_fields = len(cursor.description)
-field_names = [i[0] for i in cursor.description]
-print field_names
-print rs[1]
+#columns = [i[0] for i in cursor.description]
+#for column in columns:
+#    print column
